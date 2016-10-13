0f9f858bc953988645d7fdd7f53629bf35e66666
jin camou and sailor winkelman <sailor@>
2016-10-12 19:07:44 -0700

    add address constructor to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [0];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
 }
 
 Contact.prototype.fullName = function() {
